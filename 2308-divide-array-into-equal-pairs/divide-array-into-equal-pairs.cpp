18142f55ce631730c5e0ad132242915101b98433
yatuk <92943471+yatuk@users.noreply.github.com>
2025-03-17 21:34:51 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (92.45%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    static bool divideArray(vector<int>& nums) {
+        bitset<501> parity=0;
+        for(int x: nums) parity.flip(x);
+        return parity.none();
+    }
+};
